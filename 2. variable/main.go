e2e11a007447dc5aed5292abb11b9727f0f0d73c
Shafi <mdshadikulislamshafi@gmail.com>
2024-04-27 22:04:23 +0600

    learning variable

---
+++ 2. variable/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var userName string = "shafi"
+	fmt.Println(userName)
+	fmt.Printf("The type of the value is: %T \n", userName)
+
+	var isLoggedin bool = true
+	fmt.Println(isLoggedin)
+	fmt.Printf("This type of value is: %T \n", isLoggedin)
+
+	var myROll int = 114097
+	fmt.Println(myROll)
+	fmt.Printf("this is a %T type data\n", myROll)
+}
