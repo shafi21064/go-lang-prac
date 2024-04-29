bffcec3dd596d894781b394dcdfa8929e53e4576
Shafi <mdshadikulislamshafi@gmail.com>
2024-04-29 21:40:02 +0600

    learning variable declaration

--- 2. variable/main.go
+++ 2. variable/main.go
@@ -3,15 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	var userName string = "shafi"
-	fmt.Println(userName)
-	fmt.Printf("The type of the value is: %T \n", userName)
+	// var userName string = "shafi"
+	// fmt.Println(userName)
+	// fmt.Printf("The type of the value is: %T \n", userName)
 
-	var isLoggedin bool = true
-	fmt.Println(isLoggedin)
-	fmt.Printf("This type of value is: %T \n", isLoggedin)
+	// var isLoggedin bool = true
+	// fmt.Println(isLoggedin)
+	// fmt.Printf("This type of value is: %T \n", isLoggedin)
 
 	var myROll int = 114097
-	fmt.Println(myROll)
-	fmt.Printf("this is a %T type data\n", myROll)
+	// fmt.Println(myROll)
+	// fmt.Printf("this is a %T type data\n", myROll)
+	fmt.Print(myROll)
+	// fmt.Printf(myROll);
+
+	/// No var keyword variable
+	myName := "Shafi"
+	fmt.Println(myName)
+
+	var s, b, c int = 4, 10, 60
+
+	fmt.Println(s)
+	fmt.Println(b)
+	fmt.Println(c)
+
 }
