6186d3b9ec9338018768328c1d5716a1cdfa4541
Shafi <mdshadikulislamshafi@gmail.com>
2024-05-02 00:32:41 +0600

    conversion

--- 3userinput/main.go
+++ 3userinput/main.go
@@ -4,15 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Println("User Input File")
 
-	fmt.Println("Enter your Name: ")
+	fmt.Println("Enter your roll: ")
 	reader := bufio.NewReader(os.Stdin)
 
 	input, _ := reader.ReadString('\n')
 
-	fmt.Printf("Thank you %v for your response", input)
+	fmt.Println("Thank you for your response, Your roll is", input)
+
+	///Conversion
+	numAdd, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(numAdd + 1)
+	}
+
 }
