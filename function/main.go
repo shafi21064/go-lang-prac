9e03698b455062164a0de13687659d76e8624385
Shafi <mdshadikulislamshafi@gmail.com>
2025-04-01 16:03:56 +0600

    worked on function

--- function/main.go
+++ function/main.go
@@ -4,22 +4,41 @@ import "fmt"
 
 func main() {
 	//fmt.Println(adder(10, 20))
-	result, error := divider(20, 40)
+	// result, error := divider(20, 40)
 
-	if error != nil {
-		fmt.Println("Error:", error)
-	} else {
-		fmt.Println("Result:", result)
-	}
+	// if error != nil {
+	// 	fmt.Println("Error:", error)
+	// } else {
+	// 	fmt.Println("Result:", result)
+	// }
+
+	fmt.Println(sum(1, 2, 3, 4, 5))
 }
 
+// Single return function
 func adder(a, b int) int {
 	return a + b
 }
 
+// Multi return function
 func divider(a, b float32) (float32, error) {
 	if b == 0 {
 		return 0, fmt.Errorf("Can't divided by zero")
 	}
 	return a / b, nil
+}
+
+// Named return function
+func add(a, b int) (sum int) {
+	sum = a + b
+	return
+}
+
+// Variadic function
+func sum(num ...int) int {
+	total := 0
+	for _, item := range num {
+		total += item
+	}
+	return total
 }
